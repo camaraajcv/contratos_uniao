b78fe554385057c8d416da9f907a433c69a0ce56
camaraajcv <123605493+camaraajcv@users.noreply.github.com>
2026-01-29 19:22:17 +0000

    zxczxc

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,25 @@
 import streamlit as st
+import requests
 
 API_TOKEN = st.secrets["PORTAL_TRANSPARENCIA_TOKEN"]
 
-BASE_URL = "https://api.portaldatransparencia.gov.br/api-de-dados/contratos"
-st.write("Token carregado?", bool(st.secrets.get("PORTAL_TRANSPARENCIA_TOKEN")))
+url = "https://api.portaldatransparencia.gov.br/api-de-dados/contratos"
+
+headers = {
+    "chave-api-dados": API_TOKEN
+}
+
+params = {
+    "pagina": 1
+}
+
+response = requests.get(url, headers=headers, params=params)
+
+st.write("Status code:", response.status_code)
+
+if response.status_code == 200:
+    dados = response.json()
+    st.write("Quantidade de registros:", len(dados))
+    st.json(dados[0])
+else:
+    st.error(response.text)
